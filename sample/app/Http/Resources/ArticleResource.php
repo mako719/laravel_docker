5cbd3dc1cc21f79c9bba49f8a235af718afdec60
mako(Java2022.11) <118739580+mako719@users.noreply.github.com>
2024-01-14 18:33:33 +0900

    CHAPTER4 APIリソース実装

---
+++ sample/app/Http/Resources/ArticleResource.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Collection;
+
+use function sprintf;
+
+class ArticleResource extends JsonResource
+{
+    public static $wrap = '';
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->resource['id'],
+            'title' => $this->resource['title'],
+            '_embedded' => [
+                'comments' => new CommentResourceCollection(
+                    new Collection($this->resource['comments'])
+                ),
+                'user' => new UserResource(
+                    [
+                        'user_id' => $this->resource['user_id'],
+                        'user_name' => $this->resource['user_name']
+                    ]
+                ),
+            ],
+            '_links' => [
+                'self' => [
+                    'href' => sprintf(
+                        'https://example.com/articles/%s',
+                        $this->resource['id']
+                    )
+                ]
+            ]
+        ];
+    }
+}
